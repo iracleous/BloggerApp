ba824c886799509cfa1bfb67a12cef623212b021
Dimitris Iracleous <diracleous@athtech.gr>
2024-11-08 11:02:18 +0200

    migration

--- BloggerApp/Context/BlogDbContext.cs
+++ BloggerApp/Context/BlogDbContext.cs
@@ -6,4 +6,17 @@ namespace BloggerApp.Context;
 public class BlogDbContext : DbContext
 {
     public DbSet<Post> Posts { get; set; }
+
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Set up your connection string and other configuration here
+        optionsBuilder.UseSqlServer("Server=localhost; Initial Catalog = Plaisio_EF; Trusted_Connection =true; TrustServerCertificate=true");
+
+        // Enable sensitive data logging (only in development)
+        optionsBuilder.EnableSensitiveDataLogging();
+
+        // Additional configurations as needed
+    }
 }
